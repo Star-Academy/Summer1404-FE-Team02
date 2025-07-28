39a545b9a57fa083b46180544b73090614546f13
Masih Abedini <masih7816@gmail.com>
2025-07-28 12:42:33 +0330

    refactor: reorganize project structure and remove unused test files

--- phase-6/book-store/src/app.routes.ts
+++ phase-6/book-store/src/app.routes.ts
@@ -1,9 +1,9 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from "./app/home/home.component";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  }
+  },
 ];

20939a5d65d481503994142675bba8264625334a
Masih Abedini <masih7816@gmail.com>
2025-08-02 17:18:31 +0330

    refactor: clean up imports and format main.ts

--- phase-7/src/main.ts
+++ phase-7/src/main.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {importProvidersFrom} from "@angular/core";
-import {HttpClientModule, provideHttpClient, withFetch} from "@angular/common/http";
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(withFetch())]
-})
+  providers: [provideHttpClient(withFetch())],
+});

db3822e831ca7fd5bfd5ebbf751ee3f3588e172b
Masih Abedini <masih7816@gmail.com>
2025-08-02 16:51:24 +0330

    add Q2 component and integrate into app component

--- phase-7/src/app/app.component.ts
+++ phase-7/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Q1Component} from "./q1/q1.component";
+import { Q1Component } from './q1/q1.component';
+import { Q2Component } from './q2/q2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Q1Component],
+  imports: [Q1Component, Q2Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'phase-7';

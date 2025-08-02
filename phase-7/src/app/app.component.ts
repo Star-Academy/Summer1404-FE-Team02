2657956314caba84952314d68622991c6b7f7119
Mohammad Amin Dadkhah <mohammadamin.dadkhah13@gmail.com>
2025-08-02 16:39:40 +0330

    q1 added

--- phase-7/src/app/app.component.ts
+++ phase-7/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Q1Component} from "./q1/q1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Q1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

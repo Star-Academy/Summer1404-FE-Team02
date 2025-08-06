a13683bd6c1d91d3a150a79abd4ec074ba305890
Masih Abedini <masih7816@gmail.com>
2025-08-06 11:36:52 +0330

    chore: init new phase-10 project

---
+++ phase-10/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import {Routes} from '@angular/router';
+import {ContactComponent} from './pages/contact/contact.component';
+import {AboutComponent} from './pages/about/about.component';
+
+export const routes: Routes = [
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  }
+];
